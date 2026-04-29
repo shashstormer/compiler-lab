04a70024cb9f76957d52f64f4d0fa2b28f381d9d
shashstormer <shashanka5398@gmail.com>
2026-04-30 00:46:22 +0530

    added autoclean gen files and slight improvements in flag handling in run.sh

--- chart_assignment/run.sh
+++ chart_assignment/run.sh
@@ -1,47 +1,75 @@
 #!/bin/bash
 cd "/home/shash/Desktop/srm/compiler_design/chart_assignment"
 
-function show_help {
-    echo "Usage: ./run.sh [option]"
-    echo ""
-    echo "Options:"
-    echo "  code          Compile and execute the source program (main.c)"
-    echo "  compile       Compile and run the compiler"
-    echo "  compile dead  Compile and run with optimizations"
-    echo "  clean         Delete generated files"
-    echo "  help          Show this help message"
+MODE=""
+OPTIMIZE=false
+CLEAN_UP=true
+
+for arg in "$@"; do
+    case "$arg" in
+        code)    MODE="code" ;;
+        compile) MODE="compile" ;;
+        dead)    OPTIMIZE=true ;;
+        noclean) CLEAN_UP=false ;;
+        clean)   MODE="manual_clean" ;;
+        help)    MODE="help" ;;
+    esac
+done
+
+function do_clean {
+    rm -f compiler program lex.yy.c parser.tab.c parser.tab.h main real_compiler compiler_sim
 }
 
-case "$1" in
-    code)
-        gcc main.c -o program
-        if [ $? -eq 0 ]; then
-            ./program
+function show_help {
+    echo "Usage: ./run.sh [options]"
+    echo ""
+    echo "Actions:"
+    echo "  code      Execute main.c directly"
+    echo "  compile   Run the compiler pipeline"
+    echo "  clean     Manual cleanup"
+    echo ""
+    echo "Flags:"
+    echo "  dead      Enable compiler optimizations"
+    echo "  noclean   Keep generated files after execution"
+    echo "  help      Show this menu"
+}
+
+if [ -z "$MODE" ] || [ "$MODE" == "help" ]; then
+    show_help
+    exit 0
+fi
+
+if [ "$MODE" == "manual_clean" ]; then
+    echo "Cleaning up..."
+    do_clean
+    exit 0
+fi
+
+if [ "$MODE" == "code" ]; then
+    gcc main.c -o program
+    if [ $? -eq 0 ]; then
+        ./program
+    else
+        echo "Compilation failed."
+    fi
+fi
+
+if [ "$MODE" == "compile" ]; then
+    bison -d parser.y
+    flex lexer.l
+    gcc lex.yy.c parser.tab.c compiler.c -o compiler
+    
+    if [ $? -eq 0 ]; then
+        if [ "$OPTIMIZE" = true ]; then
+            ./compiler dead
         else
-            echo "Compilation failed."
+            ./compiler
         fi
-        ;;
-    compile)
-        bison -d parser.y
-        flex lexer.l
-        gcc lex.yy.c parser.tab.c compiler.c -o compiler
-        
-        if [ $? -eq 0 ]; then
-            if [ "$2" == "dead" ]; then
-                ./compiler dead
-            else
-                ./compiler
-            fi
-        else
-            echo "Compilation failed."
-        fi
-        ;;
-    clean)
-        echo "Cleaning up generated files..."
-        rm -f compiler program lex.yy.c parser.tab.c parser.tab.h main real_compiler
-        echo "Done."
-        ;;
-    help|*)
-        show_help
-        ;;
-esac
+    else
+        echo "Compilation failed."
+    fi
+fi
+
+if [ "$CLEAN_UP" = true ]; then
+    do_clean
+fi
